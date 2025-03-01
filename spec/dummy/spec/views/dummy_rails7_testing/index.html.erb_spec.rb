5ace5e8f3284ac0512b9a8b138394e104616cee8
captproton <carl@wdwhub.net>
2025-03-01 15:06:15 -0800

    Disable unnecessary dummy app specs
    - Marked dummy app request specs as skipped since they're not critical for gem functionality
    - Disabled helper and view specs by adding skip tag
    - Added run_all_specs.sh script that excludes dummy app specs
    - This cleans up the test output and focuses on the actual gem functionality

--- spec/dummy/spec/views/dummy_rails7_testing/index.html.erb_spec.rb
+++ spec/dummy/spec/views/dummy_rails7_testing/index.html.erb_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "dummy_rails7_testing/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+# Skip view specs as they're not needed for the gem's testing
+RSpec.describe "dummy_rails7_testing/index.html.erb", type: :view, skip: "Not needed for gem testing" do
+  # These tests are not relevant to the gem's functionality
 end
