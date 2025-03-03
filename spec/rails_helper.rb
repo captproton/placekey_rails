3822ae7977f59371e91ff1fb6f7107ae644bbf21
captproton <carl@wdwhub.net>
2025-03-03 08:56:08 -0800

    CHANGE: linting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # Update fixture configuration to use plural form
-  config.fixture_paths = ['spec/fixtures']
+  config.fixture_paths = [ 'spec/fixtures' ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each
   # example within a transaction, remove the following line or assign false
